2ae174a0fe4ae1f005824824e893d33b4dedcf0a
Anik Paul Gomes <gomesap29@gmail.com>
2020-02-11 22:13:02 -0600

    working log in page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { TopNavbarComponent } from "./top-navbar/top-navbar.component";
 import { LogInComponent } from "./log-in/log-in.component";
+import { StorageServiceModule } from "ngx-webstorage-service";
 
 @NgModule({
   declarations: [TopNavbarComponent, LogInComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, StorageServiceModule],
   providers: [],
   bootstrap: [LogInComponent]
 })
--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IUser } from "../user";
 
 @Component({
-  selector: 'app-log-in',
-  templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.css']
+  selector: "app-log-in",
+  templateUrl: "./log-in.component.html",
+  styleUrls: ["./log-in.component.css"]
 })
 export class LogInComponent implements OnInit {
+  email: string = "";
+  password: string = "";
+  emailR: string = "";
+  passwordR: string = "";
+  nameR: string = "";
+  user: IUser;
+  isRegisterd: boolean = true;
+  message: string = "";
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit() {
+  logIn(): void {
+    this.message = "";
+    if (this.email == "" || this.password == "") {
+      this.message = "Please fill out your email and password";
+    } else {
+      this.user = JSON.parse(localStorage.getItem(this.email));
+      if (this.user == null) {
+        this.message = "Invalid credentials";
+      } else if (this.user.password != this.password) {
+        this.message = "Invalid credentials";
+      } else {
+        this.message = "You are good";
+      }
+    }
   }
 
+  signUp(): void {
+    this.message = "";
+    this.isRegisterd = false;
+  }
+  registarUser(): void {
+    this.message = "";
+    if (this.nameR == "" || this.emailR == "" || this.passwordR == "") {
+      this.message = "Please fill out all the above information to registar";
+    } else {
+      this.user = {
+        name: this.nameR,
+        email: this.emailR,
+        password: this.passwordR
+      };
+      localStorage.setItem(this.emailR, JSON.stringify(this.user));
+      this.emailR = "";
+      this.nameR = "";
+      this.passwordR = "";
+      this.isRegisterd = true;
+    }
+  }
+  logInBack(): void {
+    this.message = "";
+    this.isRegisterd = true;
+  }
 }
