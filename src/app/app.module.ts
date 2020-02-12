11e816cec895987a5851f3909eec594bd50b34bf
Anik Paul Gomes <gomesap29@gmail.com>
2020-02-12 12:01:03 -0600

    removed unnecessary imports which was causing compiling error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { TopNavbarComponent } from './top-navbar/top-navbar.component';
-import { LogInComponent } from './log-in/log-in.component';
-import { StorageServiceModule } from 'angular-webstorage-service';
+import { AppRoutingModule } from "./app-routing.module";
+import { TopNavbarComponent } from "./top-navbar/top-navbar.component";
+import { LogInComponent } from "./log-in/log-in.component";
 
 @NgModule({
   declarations: [TopNavbarComponent, LogInComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, StorageServiceModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [LogInComponent]
 })
