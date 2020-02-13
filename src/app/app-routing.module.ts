8e6f8342d6f25881927242335cb3b50427d83897
Anik Paul Gomes <gomesap29@gmail.com>
2020-02-12 21:24:03 -0600

    routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { NutritionComponent } from "./nutrition/nutrition.component";
+import { LogInComponent } from "./log-in/log-in.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "nutrition", component: NutritionComponent },
+  { path: "signin", component: LogInComponent },
+  { path: "", redirectTo: "signin", pathMatch: "full" },
+  { path: "**", redirectTo: "signin", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { TopNavbarComponent } from "./top-navbar/top-navbar.component";
 import { LogInComponent } from "./log-in/log-in.component";
+import { NutritionComponent } from "./nutrition/nutrition.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [TopNavbarComponent, LogInComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [TopNavbarComponent, LogInComponent, NutritionComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, RouterModule],
   providers: [],
-  bootstrap: [LogInComponent]
+  bootstrap: [TopNavbarComponent]
 })
 export class AppModule {}
